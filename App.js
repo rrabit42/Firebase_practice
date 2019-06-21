76b7be4fef8e33232744e577c95c47be4a2b113f
rrabit42 <gegiraffe@naver.com>
2019-06-22 03:11:43 +0900

    [추가] firebase의 데이터 불러오기

--- App.js
+++ App.js
@@ -7,22 +7,55 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+//패키지 연결
+import firebase from 'firebase';
 
 export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { data: [] } //Firebase 데이터가 저장될 공간
+  }
+  componentDidMount(){
+    //파이어베이스 연결 코드
+    firebase.initializeApp({
+        apiKey: "AIzaSyCMG92fTz1JOMkt-i1A-caJlAQHj5zAL3w",
+        authDomain: "practice-690dd.firebaseapp.com",
+        databaseURL: "https://practice-690dd.firebaseio.com",
+        projectId: "practice-690dd",
+        storageBucket: "",
+        messagingSenderId: "634917161579",
+        appId: "1:634917161579:web:93f62e0634d54500"
+    });
+
+    //저장된 데이터 읽기
+    const ref = firebase.database().ref();
+
+    ref.on("value", snapshot => {
+      //Firebase 데이터 state.data로 업데이트
+      this.setState({ data: snapshot.val()});
+    })
+
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        {
+          //json파일은 map()을 사용하여 화면에 노출시킨다
+          this.state.data.map(value =>{
+            return(
+              <View>
+                <Image
+                  style = {{width: 110, height: 110}}
+                  source ={{uri : value.image}}
+                />
+                <Text>{value.name}</Text>
+              </View>
+            )
+          })
+        }
       </View>
     );
   }
@@ -34,15 +67,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
